bc11da6bdde9e96ed76ff368d5357ad16ac4baf1
Rhebeca Abreu <rhebecaabreu@gmail.com>
2019-03-30 18:54:45 -0400

    Arrumando código

--- ProxyThread.java
+++ ProxyThread.java
@@ -33,109 +33,94 @@ public class ProxyThread implements Runnable {
       proxyToClientBw = new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
 
       requestString = proxyToClientBr.readLine();
-
-      // Get the Request type
-      String request = requestString.substring(0, requestString.indexOf(' '));
-      // remove request type and space
-      String urlString = requestString.substring(requestString.indexOf(' ') + 1);
-
-      String http = urlString.substring(urlString.indexOf(' '));
-
-      // Remove everything past next space
-      urlString = urlString.substring(0, urlString.indexOf(' '));
-      // Prepend http:// if necessary to create correct URL
-      if (!urlString.substring(0, 4).equals("http")) {
-        String temp = "http://";
-        urlString = temp + urlString;
-      }
+     
+      StringTokenizer tokenizedLine = new StringTokenizer(requestString);
       
-      if (request.equals("GET")) {
-        System.out.println("Request Received " + requestString); 
-        // ProxyCache p = new ProxyCache(proxyToClientBw, proxyToClientBr, socketClient);
-        // p.createCacheFile();
-        // File file;
-        // if ((file = p.getCachedPage(urlString)) != null) {
-        //   System.out.println("Cached Copy found for : " + urlString + "\n");
-        //   p.sendCachedPageToClient(file);
-        // } else {
-          sendData(urlString);
-        
+      String request="", http="", urlString="";
+      while(tokenizedLine.hasMoreTokens()) { 
+        request = tokenizedLine.nextToken().toString();
+        urlString = tokenizedLine.nextToken().toString();
+        http = tokenizedLine.nextToken().toString();
+
+        if (!urlString.substring(0, 4).equals("http")) {
+          String temp = "http://";
+          urlString = temp + urlString;
+        }
+
+        if (request.equals("GET")) {
+          System.out.println("Request Received " + requestString);
+          sendData(urlString, request, http);
+        } else {
+          System.out.println("Bad Request Message");
+        }
+
+        request = null;
+        urlString = null;
+        http = null;
+
       }
 
     } catch (IOException e) {
       e.printStackTrace();
     }
-    // } catch (ClassNotFoundException e) {
-    //   // TODO Auto-generated catch block
-    //   e.printStackTrace();
-    // }
-   
   }
 
-  public void sendData(String urlString) {
+  
+  public String request(String urlString, String requestType, String httpVersion) {
+    String line = requestType + " / " + httpVersion + "\r\n" +
+                  "Host: "+ urlString + "\r\n" +
+                  "Connection: close\n\r\n";
+    System.out.println(line);
 
+    return line;
+  }
+
+
+  public void sendData(String urlString, String requestType, String httpVersion) {
     try {
-      // Compute a logical file name as per schema
-      // This allows the files on stored on disk to resemble that of the URL it was
-      // taken from
-      int fileExtensionIndex = urlString.lastIndexOf(".");
-      String fileExtension;
+      String[]file = fileName(urlString);
+      String fileName=file[0];
+      String fileExtension=file[1];
 
-      // Get the type of file
-      fileExtension = urlString.substring(fileExtensionIndex, urlString.length());
-
-      // Get the initial file name
-      String fileName = urlString.substring(0, fileExtensionIndex);
-
-      // Trim off http://www. as no need for it in file name
-      fileName = fileName.substring(fileName.indexOf('.') + 1);
-
-      // Remove any illegal characters from file name
-      fileName = fileName.replace("/", "__");
-      fileName = fileName.replace('.', '_');
-
-      // Trailing / result in index.html of that directory being fetched
-      if (fileExtension.contains("/")) {
-        fileExtension = fileExtension.replace("/", "__");
-        fileExtension = fileExtension.replace('.', '_');
-        fileExtension += ".html";
-      }
-
-      fileName = fileName + fileExtension;
-      
       if(!checkIfIsImage(fileExtension, urlString, fileName)) {
         
         // Create the URL
         URL remoteURL = new URL(urlString);
+
         // Create a connection to remote server
         HttpURLConnection proxyToServerCon = (HttpURLConnection) remoteURL.openConnection();
         proxyToServerCon.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
         proxyToServerCon.setRequestProperty("Content-Language", "en-US");
         proxyToServerCon.setUseCaches(false);
         proxyToServerCon.setDoOutput(true);
-
-        // Create Buffered Reader from remote Server
+        // System.out.println(proxyToServerCon.getHeaderFields());
+        
         BufferedReader proxyToServerBR = new BufferedReader(new InputStreamReader(proxyToServerCon.getInputStream()));
 
-        // Send success code to client
-        String line = "HTTP/1.1 200 OK\n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
-        proxyToClientBw.write(line);
-
-        // Read from input stream between proxy and remote server
-        while ((line = proxyToServerBR.readLine()) != null) {
-          // Send on data to client
+        if(proxyToServerCon.getResponseCode()==200) {
+          // Send success code to client
+          String line = "HTTP/1.1 200 OK\n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
           proxyToClientBw.write(line);
-        }
 
-        // Ensure all data is sent by this point
-        proxyToClientBw.flush();
+          System.out.println("\nHTTP/1.1 " + proxyToServerCon.getResponseCode() + " OK");
+          System.out.println("Content-Length: " + proxyToServerCon.getContentLength());
+          System.out.println("Content-Type: " + proxyToServerCon.getContentType()+"\r\n");
 
-        // Close Down Resources
-        if (proxyToServerBR != null) {
-          proxyToServerBR.close();
+          // Read from input stream between proxy and remote server
+          while ((line = proxyToServerBR.readLine()) != null) {
+            // Send on data to client
+            proxyToClientBw.write(line);
+          }
+
+          // Ensure all data is sent by this point
+          proxyToClientBw.flush();
+
+          // Close Down Resources
+          if (proxyToServerBR != null) {
+            proxyToServerBR.close();
+          }
         }
       }
-
       if (proxyToClientBw != null) {
         proxyToClientBw.close();
       }
@@ -165,15 +150,47 @@ public class ProxyThread implements Runnable {
 
       } else {
         System.out.println("Sending 404 to client as image wasn't received from server" + fileName);
-        String error = "HTTP/1.1 404 NOT FOUND\n" + "Proxy-agent: Proxy/1.0\n" + "\r\n";
+        String error = "HTTP/1.1 404 NOT FOUND\r\n" + "Proxy-agent: Proxy/1.0\n" + "\r\n";
         proxyToClientBw.write(error);
         proxyToClientBw.flush();
         return false; 
       }
-
       return true;
     } else {
       return false; 
     }
+  }
+
+  public String[] fileName(String urlString) {
+    // Compute a logical file name as per schema
+    // This allows the files on stored on disk to resemble that of the URL it was
+    // taken from
+    int fileExtensionIndex = urlString.lastIndexOf(".");
+    String fileExtension;
+
+    // Get the type of file
+    fileExtension = urlString.substring(fileExtensionIndex, urlString.length());
+
+    // Get the initial file name
+    String fileName = urlString.substring(0, fileExtensionIndex);
+
+    // Trim off http://www. as no need for it in file name
+    fileName = fileName.substring(fileName.indexOf('.') + 1);
+
+    // Remove any illegal characters from file name
+    fileName = fileName.replace("/", "__");
+    fileName = fileName.replace('.', '_');
+
+    // Trailing / result in index.html of that directory being fetched
+    if (fileExtension.contains("/")) {
+      fileExtension = fileExtension.replace("/", "__");
+      fileExtension = fileExtension.replace('.', '_');
+      fileExtension += ".html";
+    }
+    String[] file = new String[2];
+    file[0]=fileName = fileName + fileExtension;; 
+    file[1]=fileExtension;
+
+    return file;
   }
 }
